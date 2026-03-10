975b62e1d30e835fe5ad59516e924fade2984fca
Louisc@100 <honeybadgerapps5@gmail.com>
2026-03-10 06:42:26 +0200

    Enable Stripe token purchases using dynamic priceId

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -2,37 +2,39 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
- apiVersion: "2026-02-25.clover",
+  apiVersion: "2026-02-25.clover",
 });
 
 export async function POST(req: Request) {
   try {
-    const { uid, email } = await req.json();
+    const { uid, email, priceId, mode } = await req.json();
 
     const session = await stripe.checkout.sessions.create({
-      mode: "subscription",
+      mode: mode || "payment",
+
       payment_method_types: ["card"],
 
       line_items: [
         {
-          price: "price_1T8a8PCplvzmJJBy8krfB9Gq",
+          price: priceId,
           quantity: 1,
         },
       ],
 
       metadata: {
-        uid: uid,
-        email: email,
+        uid,
+        email,
+        priceId,
       },
 
       success_url: "https://golf-challenge-pwa.vercel.app/dashboard",
-cancel_url: "https://golf-challenge-pwa.vercel.app/register",
+      cancel_url: "https://golf-challenge-pwa.vercel.app/dashboard",
     });
 
     return NextResponse.json({ url: session.url });
 
- } catch (error: any) {
-  console.error("Stripe error:", error);
-  return NextResponse.json({ error: error.message });
-}
+  } catch (error: any) {
+    console.error("Stripe error:", error);
+    return NextResponse.json({ error: error.message });
+  }
 }

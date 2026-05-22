c7e6918f5d043fd14273c0c3a258e4c93e1200a7
Louisc@100 <honeybadgerapps5@gmail.com>
2026-05-22 18:36:12 +0200

    Adjust landing page hero image height for mobile fit

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -28,7 +28,7 @@ export default function LandingPage() {
             <img
               src="/app_app_main.png"
               alt="Teez Dashboard"
-              className="w-full h-[50vh] md:h-[60vh] object-contain object-center"
+              className="w-full h-[20vh] md:h-[30vh] object-contain object-center"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
           </div>
